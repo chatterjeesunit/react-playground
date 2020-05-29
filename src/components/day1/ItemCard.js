8d6b33ccd9bab5e245f4703510a3c4e25ccde30f
Sunit Chatterjee <sunit_chatterjee@external.mckinsey.com>
2020-05-29 08:21:47 +0530

    Initial commit

---
+++ src/components/day1/ItemCard.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class ItemCard extends Component {
+
+    render() {
+       return (
+        <div>
+            <h1>Testing React</h1>
+        </div>
+       );
+    }
+    
+}
+
+
+export default ItemCard
